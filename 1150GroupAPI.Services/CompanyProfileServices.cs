e5d26826304ead0c7d665c5dffff1270da3aecde
Karina Rodriguez <78121736+macrolypoly@users.noreply.github.com>
2021-05-13 20:24:38 -0400

    K1 - K5, started on K6

---
+++ 1150GroupAPI.Services/CompanyProfileServices.cs
@@ -0,0 +1,31 @@
+﻿using _1150GroupAPI.Data;
+using _1150GroupAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1150GroupAPI.Services
+{
+    public class CompanyProfileServices
+    {
+        private Guid _userID;
+        public CompanyProfileServices(Guid userid)
+        {
+            _userID = userid;
+        }
+        public bool CreateCompany(CompanyProfileCreate model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var companyentity = new CompanyProfile();
+                companyentity.CompanyID = model.CompanyID;
+                companyentity.CompanyName = model.CompanyName;
+
+                ctx.Companies.Add(companyentity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
