bff036f9c99b55efcdf2d0c6f72a9c905d783aad
Karina Rodriguez <78121736+macrolypoly@users.noreply.github.com>
2021-05-15 10:14:07 -0400

    category class with properties

--- 1150GroupAPI.Data/Category.cs
+++ 1150GroupAPI.Data/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace _1150GroupAPI.Data
 {
     public class Category
     {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        [ForeignKey(nameof(CompanyID))]
+        public int CompanyID { get; set; }
     }
 }
