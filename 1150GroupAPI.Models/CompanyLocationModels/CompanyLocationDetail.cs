62774ff39e5faba38c1e7edd4290cb431652a2a1
Max Hahn <max.vhahn@gmail.com>
2021-05-15 10:24:02 -0400

    Built CompanyLocationDetail model

--- 1150GroupAPI.Models/CompanyLocationModels/CompanyLocationDetail.cs
+++ 1150GroupAPI.Models/CompanyLocationModels/CompanyLocationDetail.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _1150GroupAPI.Models.CompanyLocationModels
 {
-    class CompanyLocationDetail
+    public class CompanyLocationDetail
     {
+        public int LocationID { get; set; }
+
+        public string Address
+        {
+            get
+            {
+                return (Street + " " + City + "," + State + " " + Zip);
+            }
+        }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public int Zip { get; set; }
     }
 }
