ddbbf18c0c1b6d7ffcb1f517c32c44f4e934550c
Nasser <adegboyefoluso@yahoo.com>
2021-05-18 10:20:56 -0400

    commit jobcontroller

---
+++ 1150GroupAPI/Controllers/JobController.cs
@@ -0,0 +1,22 @@
+﻿using _1150GroupAPI.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _1150GroupAPI.Controllers
+{
+    public class JobController : ApiController
+    {
+
+        private JobServices CreateJobServices()
+        {
+            var userid = Guid.Parse(User.Identity.GetUserId());
+            var jservices = new JobServices(userid);
+            return jservices;
+        }
+    }
+}
