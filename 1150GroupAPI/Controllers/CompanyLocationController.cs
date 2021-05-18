406b7d9a4ee31320fecc206564f3331ef8e2d0f5
Karina Rodriguez <78121736+macrolypoly@users.noreply.github.com>
2021-05-18 19:55:43 -0400

    controller

--- 1150GroupAPI/Controllers/CompanyLocationController.cs
+++ 1150GroupAPI/Controllers/CompanyLocationController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using _1150GroupAPI.Models;
+using _1150GroupAPI.Models.CompanyLocationModels;
+using _1150GroupAPI.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +11,57 @@ using System.Web.Http;
 
 namespace _1150GroupAPI.Controllers
 {
+    [Authorize]
     public class CompanyLocationController : ApiController
     {
+        private CompanyLocationServices CreateCompanyLocationService()
+        {
+            Guid userid = Guid.Parse(User.Identity.GetUserId());
+            var companyLocationService = new CompanyLocationServices();
+            return companyLocationService;
+        }
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            CompanyLocationServices companyLocationServices = CreateCompanyLocationService();
+            var companies = companyLocationServices.GetCompanyLocationById(id);
+            return Ok(companies);
+        }
+        [HttpPost]
+        public IHttpActionResult Post(CompanyLocationCreate companyLocation)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCompanyLocationService();
+
+            if (!service.CreateCompanyLocation(companyLocation))
+                return InternalServerError();
+
+            return Ok();
+        }
+        [HttpPut]
+        public IHttpActionResult Put(CompanyLocationEdit company)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCompanyLocationService();
+
+            if (!service.UpdateCompanyLocation(company))
+                return InternalServerError();
+
+            return Ok();
+        }
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateCompanyLocationService();
+
+            if (!service.DeleteCompanyLocation(id))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
